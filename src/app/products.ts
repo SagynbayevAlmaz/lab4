fda7d0be72bab707282ce165fa9ae12b24fa39d2
Allmaz <almaizk0201@gmail.com>
2021-02-24 16:11:35 +0500

    Lab4

---
+++ src/app/products.ts
@@ -0,0 +1,92 @@
+export const products = [
+  {
+    id: 1,
+    name: 'AMD Ryzen 5 3600 6-Core',
+    price: 200,
+    description: 'AMD Ryzen 5 3600 6-Core, 12-Thread Unlocked Desktop Processor with Wraith Stealth Cooler',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71WPGXQLcLL._AC_SL1384_.jpg',
+    rating: 4.5,
+    link: 'https://www.amazon.com/AMD-Ryzen-3600-12-Thread-Processor/dp/B07STGGQ18/ref=lp_16225007011_1_11'
+  },
+  {
+    id: 2,
+    name: 'AUKEY USB C Hub',
+    price: 69,
+    description: 'AUKEY USB C Hub 12-in-1 Type C Adapter with Ethernet, 4K HDMI,VGA,2 USB 3.0,2 USB 2.0,100W PD,USB-C Data Port and SD/TF',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/61bSkqFmJKL._AC_SL1500_.jpg',
+    rating:4.7,
+    link: 'https://www.amazon.com/AUKEY-Adapter-Ethernet-MacBookPro-Thunderbolt/dp/B0841T9KC9/ref=sr_1_1_sspa?crid=3GK5NELWK4LPD&dchild=1&keywords=usb+c+hub&qid=1614148279&s=computers-intl-ship&sprefix=usb%2Ccomputers-intl-ship%2C323&sr=1-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyOVhHMDg2ME1PTFRJJmVuY3J5cHRlZElkPUEwNjkwODAzOVk0MFM5SDJRUEZOJmVuY3J5cHRlZEFkSWQ9QTA5OTQ4ODM3WEhUUUU0NEZWSzUmd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl'
+  },
+  {
+    id: 3,
+    name: 'Sony WH1000XM3',
+    price: 350,
+    description: 'Sony WH1000XM3 Noise Cancelling Headphones, Wireless Bluetooth Over the Ear Headset – Black',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/519EahgX90L._AC_SL1500_.jpg',
+    rating: 4,
+    link: 'https://www.amazon.com/Sony-Noise-Cancelling-Headphones-WH1000XM3/dp/B07G4MNFS1/ref=sr_1_1?crid=2VP05BCLJXOQZ&dchild=1&keywords=sony+mdr+1000x&qid=1614149160&sprefix=sony+mdr+100%2Caps%2C316&sr=8-1'
+  },
+  {
+    id: 4,
+    name: 'Apple iPad Pro',
+    price: 1000,
+    description: '2020 Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Space Gray (4th Generation)',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81SGb5l%2BlZL._AC_SL1500_.jpg',
+    rating: 4.5,
+    link: 'https://www.amazon.com/Apple-iPad-12-9-inch-Wi-Fi-128GB/dp/B0863D15R6?ref_=ast_sto_dp'
+  },
+  {
+    id: 5,
+    name: 'New Apple MacBook Pro',
+    price: 1300,
+    description: 'New Apple MacBook Pro with Apple M1 Chip (13-inch, 8GB RAM, 256GB SSD Storage) - Space Gray',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71an9eiBxpL._AC_SL1500_.jpg',
+    rating: 4.4,
+    link: 'https://www.amazon.com/Apple-MacBook-13-inch-256GB-Storage/dp/B08N5N6RSS?ref_=ast_sto_dp'
+  },
+  {
+    id: 6,
+    name: 'GeForce GTX 1660 Ti',
+    price: 800,
+    description: 'EVGA GeForce GTX 1660 Ti SC Ultra Gaming, 6GB GDDR6, Dual Fan',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81lrF-tHYPL._AC_SL1500_.jpg',
+    rating: 4.1,
+    link: 'https://www.amazon.com/EVGA-GeForce-Ultra-Gaming-06G-P4-1667-KR/dp/B07RJGK1PW/ref=sr_1_1?crid=3BR5OQRJ38ENQ&dchild=1&keywords=rtx+3060+ti&qid=1614152034&sprefix=gtx+3060%2Caps%2C386&sr=8-1'
+  },
+  {
+    id: 7,
+    name: 'SAMSUNG QN32Q50RAFXZA Flat 32',
+    price: 500,
+    description: 'SAMSUNG QN32Q50RAFXZA Flat 32" QLED 4K 32Q50 Series Smart TV (2019 model)',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51NKhnjhpGL._AC_SL1024_.jpg',
+    rating: 4.8,
+    link: 'https://www.amazon.com/SAMSUNG-QN32Q50RAFXZA-32Q50-Smart-TV2019/dp/B07W5QYD2K/ref=sr_1_2?dchild=1&field-shipping_option-bin=3242350011&pf_rd_i=16225009011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=85a9188d-dbd5-424e-9512-339a1227d37c&pf_rd_r=J4QADFVPVVFP53X7MRV4&pf_rd_s=merchandised-search-5&pf_rd_t=101&qid=1588978526&rnid=1266092011&s=electronics&sr=1-2'
+  },
+  {
+    id: 8,
+    name: 'Oculus Quest 2',
+    price: 300,
+    description: 'Oculus Quest 2 — Advanced All-In-One Virtual Reality Headset — 64 GB',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/615YaAiA-ML._SL1500_.jpg',
+    rating: 3.7,
+    link: 'https://www.amazon.com/Oculus-Quest-Advanced-All-One-2/dp/B08F7PTF53/ref=lp_16225016011_1_1'
+  },
+  {
+    id: 9,
+    name: 'Sony PlayStation 4 Pro',
+    price: 670,
+    description: 'Sony PlayStation 4 Pro 1TB Console - Black (PS4 Pro)',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71jN27mYlhL._SL1500_.jpg',
+    rating: 4.9,
+    link: 'https://www.amazon.com/Sony-PlayStation-Pro-1TB-Console-4/dp/B07K14XKZH/ref=pd_sbs_1?pd_rd_w=D9LHE&pf_rd_p=c52600a3-624a-4791-b4c4-3b112e19fbbc&pf_rd_r=8F0GJQ14WDFJ76ZZXQ51&pd_rd_r=e8390cde-ede7-452c-9a8e-07d9c02710e8&pd_rd_wg=SYot2&pd_rd_i=B07K14XKZH&psc=1'
+  },
+  {
+    id: 10,
+    name: 'Nintendo Switch',
+    price: 350,
+    description: 'Nintendo Switch with Neon Blue and Neon Red Joy‑Con - HAC-001(-01) + SanDisk 256GB MicroSDXC UHS-I Card',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71lfEWM9AYL._AC_SL1500_.jpg',
+    rating:3.5,
+    link: 'https://www.amazon.com/Nintendo-Switch-Neon-Blue-Joy%E2%80%91/dp/B07VGRJDFY/ref=lp_16225016011_1_3?th=1'
+  }
+];
